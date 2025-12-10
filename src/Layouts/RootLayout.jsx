75722f0de270c5ec01d8a1b54a687b3b8b46578b
irfan Rakib <irfanrakib756@gmail.com>
2025-12-10 20:19:06 +0600

    initial setup done

---
+++ src/Layouts/RootLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router";
+import Footer from "../Pages/SharedPages/Footer/Footer";
+import NavBar from "../Pages/SharedPages/NavBar/NavBar";
+
+const RootLayout = () => {
+  return (
+    <div className="">
+      <div className="bg-base-100 shadow-sm">
+        {" "}
+        <NavBar />
+      </div>
+
+      <div className="container mx-auto px-4">
+        {" "}
+        <Outlet />
+      </div>
+      <div className="bg-[#0B0B0B]">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default RootLayout;
